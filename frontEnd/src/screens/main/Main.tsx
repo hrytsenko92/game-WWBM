7d378f48532d9af86faa0872caa6c38bcdad3f4b
Hrytsenko92 <m.hrytsenko1802@gmail.com>
2023-07-05 19:18:04 +0300

    add navigation, fix bugs

--- frontEnd/src/screens/main/Main.tsx
+++ frontEnd/src/screens/main/Main.tsx
@@ -1,37 +1,77 @@
 import { useState } from 'react';
-import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import styled from 'styled-components';
+import logo from '../../../public/logo.png'
+
+const Container = styled.section`
+  background-color: #92ccce;
+  border: 1px solid red;
+  height: 100vh;
+  max-width: 1200px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 100px auto;
+`;
+const Header = styled.header`
+  background-color: #b29f83;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+`;
+const Logo = styled.img`
+width: auto;
+height: 80px;
+object-position: center;
+object-fit: cover;
+`;
+const Title = styled.h3``;
+
+const LinkWrapper = styled.nav`
+display: flex;
+flex-flow: row nowrap;
+justify-content: center;
+align-items: center;
+`;
+
+const OptionLink = styled(Link)`
+display: flex;
+flex-flow: row nowrap;
+justify-content: center;
+align-items: center;
+width: 120px;
+height: 50px;
+border: 1px solid blue;
+background-color: #6c98eb;
+text-decoration: none;
+`;
+const GameLink = styled(Link)`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  height: 50px;
+  border: 1px solid blue;
+  background-color: #6c98eb;
+  text-decoration: none;
+`;
 
 export const Main = () => {
-  const navigate = useNavigate();
-  const handleExit = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  }
 
   return (
-    <>
-      <div>
-        <header>header</header>
-        <button onClick={handleExit}>Exit</button>
-        <nav>
-          <Link
-            // onClick={() => setIsOpen(false)}
-            to={'game'}
-          >
-            Game
-          </Link>
-          <Link
-            // onClick={() => setIsOpen(false)}
-            to={'options'}
-          >
-            Options
-          </Link>
-        </nav>
-        <main>
-          <Outlet />
-        </main>
-      </div>
-    </>
+    <Container>
+      <Header>
+        <LinkWrapper>
+          <GameLink to={'game'}>Гра</GameLink>
+          <OptionLink to={'options'}>Налаштування</OptionLink>
+        </LinkWrapper>
+        <Title>Хто хоче стати мільйонером?</Title>
+        <Logo src={logo} />
+      </Header>
+      <Outlet />
+    </Container>
   );
 };
--- frontEnd/src/screens/main/Options.tsx
+++ frontEnd/src/screens/main/Options.tsx
@@ -1,6 +1,85 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
+const Container = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr;
+`;
+const ScoreWrapper = styled.div`
+  background-color: #6d9689;
+  padding: 50px; // grid
+`;
+const Score = styled.div`
+  padding: 25px;
+  border: 1px solid blue;
+`; // вибірка по рейтингу, макс 10 гравців
+
+
+const SettingWrapper = styled.div`
+padding: 50px;
+background-color: #f9bb5e;
+display: grid;
+grid-template-columns: 1fr;
+grid-template-rows: auto;
+justify-items: center;
+align-items: center;
+`;
+const UserScoreWrap = styled.div`
+padding: 25px;
+display: flex;
+flex-flow: row nowrap;
+justify-content: center;
+align-items: center;
+`;
+const UserScoreData = styled.span`
+  font-size: 35px;
+`;
+const Reset = styled.button`
+  width: 100px;
+  height: 50px;
+  border: 1px solid red;
+`;
+
+const Exit = styled.button`
+  width: 100px;
+  height: 50px;
+  border: 1px solid green;
+`;
+
 export const Options: React.FC = () => {
-  return <div>Options</div>;
+  const navigate = useNavigate();
+  const handleExit = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+
+  return (
+    <Container>
+      <ScoreWrapper>
+        <Score>
+          <ul>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+            <li>10.000</li>
+          </ul>
+        </Score>
+      </ScoreWrapper>
+      <SettingWrapper>
+        <UserScoreWrap>
+          <UserScoreData>300.000 euro</UserScoreData>
+        </UserScoreWrap>
+        <Reset>Reset</Reset>
+        <Exit onClick={handleExit}>Вийти</Exit>
+      </SettingWrapper>
+    </Container>
+  );
 };
