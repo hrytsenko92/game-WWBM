d1016ea5855bf2de746bd4ec0ed227e54eabc570
Hrytsenko92 <m.hrytsenko1802@gmail.com>
2023-07-23 14:43:01 +0300

    remove vercel.json

--- backEnd/build/auth/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-export const UserSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    bestScore: { type: Number, require: false, default: 0 },
-    prevDataID: { type: [String] },
-});
-export const User = mongoose.model('User', UserSchema);
-//# sourceMappingURL=User.js.map
--- backEnd/build/modules/game/gameRouter.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-export const gameRouter = Router();
-import { GameController } from './gameController.js';
-const controller = new GameController();
-gameRouter.get('/getquestion', controller.getQuestion);
-gameRouter.put('/updateuserdata', controller.updateUserData);
-//# sourceMappingURL=gameRouter.js.map
