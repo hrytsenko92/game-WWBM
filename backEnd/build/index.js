b5495483f1700ccae8a82f206b31fad73ea7c845
Hrytsenko92 <m.hrytsenko1802@gmail.com>
2023-07-24 14:41:18 +0300

    add build

---
+++ backEnd/build/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+import cors from 'cors';
+import { authRouter } from './auth/authRouter.js';
+import { optionRouter } from './modules/options/optionRouter.js';
+import { gameRouter } from './modules/game/gameRouter.js';
+const PORT = process.env.PORT || 5000;
+const login = process.env.LOGIN;
+const password = process.env.PASSWORD;
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+};
+export const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/auth', authRouter);
+app.use('/option', optionRouter);
+app.use('/game', gameRouter);
+export const start = async () => {
+    try {
+        await mongoose.connect(`mongodb+srv://${login}:${password}@gamedataapi.btxwlgv.mongodb.net/gamedata?retryWrites=true&w=majority`);
+        app.listen(PORT, () => console.log(`server start on port: ${PORT}`));
+    }
+    catch (e) {
+        console.log(e);
+    }
+};
+start();
+//# sourceMappingURL=index.js.map
---
+++ backEnd/build/modules/options/optionRouter.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+export const optionRouter = Router();
+import { OptionController } from './optionController.js';
+const controller = new OptionController();
+optionRouter.get('/allscore', controller.getAllUsersScore);
+optionRouter.get('/userscore', controller.getUserScore);
+optionRouter.get('/resetuserscore', controller.resetUserScore);
+optionRouter.put('/updateuserscore', controller.updateUserScore);
+//# sourceMappingURL=optionRouter.js.map
